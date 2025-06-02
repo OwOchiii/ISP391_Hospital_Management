03142cabb2a191c614e0d67c77f5bbb79e14db50
BaoKhanh0230 <ptera125@gmail.com>
2025-06-02 18:23:38 +0700

    Create Necessary Classes

--- src/main/java/orochi/controller/PatientDashboardController.java
+++ src/main/java/orochi/controller/PatientDashboardController.java
@@ -1,5 +1,7 @@
 package orochi.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/patient")
 public class PatientDashboardController {
+    private static final Logger logger = LoggerFactory.getLogger(PatientDashboardController.class);
 
     @GetMapping("/dashboard")
     public String dashboard() {
---
+++ src/main/java/orochi/service/AppointmentService.java
@@ -0,0 +1,7 @@
+package orochi.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppointmentService {
+}
