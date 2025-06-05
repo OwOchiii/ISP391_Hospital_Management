93af682058f20211f37c5c718c3964e6d16a3df4
OwOchiii <nguyenvuan2005@gmail.com>
2025-06-05 22:20:19 +0700

    Fixed some

--- src/main/java/orochi/controller/DoctorAppointmentController.java
+++ src/main/java/orochi/controller/DoctorAppointmentController.java
@@ -14,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import orochi.config.CustomUserDetails;
 import orochi.model.*;
 import orochi.repository.*;
 import orochi.service.DoctorService;
@@ -576,4 +577,6 @@ public class DoctorAppointmentController {
             return "redirect:/doctor/appointments/" + appointmentId + "?doctorId=" + doctorId;
         }
     }
+
+
 }
