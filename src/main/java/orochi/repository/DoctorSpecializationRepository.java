c9c4c13923eac86bc0b1a960269100f2f91b417f
Sonlq-09 <son159a@gmail.com>
2025-06-16 00:54:20 +0700

    Update doctorSpec Repo

--- src/main/java/orochi/repository/DoctorSpecializationRepository.java
+++ src/main/java/orochi/repository/DoctorSpecializationRepository.java
@@ -1,6 +1,7 @@
 package orochi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import orochi.model.DoctorSpecialization;
@@ -12,4 +13,8 @@ public interface DoctorSpecializationRepository extends JpaRepository<DoctorSpec
 
     @Query("SELECT ds FROM DoctorSpecialization ds WHERE ds.doctor.doctorId = :doctorId")
     Optional<DoctorSpecialization> findByDoctorId(@Param("doctorId") Integer doctorId);
+
+    @Modifying
+    @Query("DELETE FROM DoctorSpecialization ds WHERE ds.specialization.specId = :specId")
+    void deleteBySpecId(@Param("specId") Integer specId);
 }
