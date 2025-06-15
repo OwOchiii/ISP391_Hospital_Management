690c7cd345100364bee96a62f3b0da605cb905a8
Sonlq-09 <son159a@gmail.com>
2025-06-16 00:57:38 +0700

    Update service repo

--- src/main/java/orochi/repository/ServiceRepository.java
+++ src/main/java/orochi/repository/ServiceRepository.java
@@ -2,9 +2,14 @@ package orochi.repository;
 
 import orochi.model.MedicalService;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ServiceRepository extends JpaRepository<MedicalService, Integer> {
-    // Không cần thêm query tùy chỉnh nếu chỉ cần CRUD cơ bản
+    @Modifying
+    @Query("DELETE FROM MedicalService s WHERE s.specialization.specId = :specId")
+    void deleteBySpecId(@Param("specId") Integer specId);
 }
