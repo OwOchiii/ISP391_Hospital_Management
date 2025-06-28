57a9e30d68c8079f6314b37385f797a87a2a4655
NguyenThanhBinh108 <binhnthe182340@fpt.edu.vn>
2025-06-29 00:56:48 +0700

    refactor: enhance Transaction model for payment processing

--- src/main/java/orochi/model/Transaction.java
+++ src/main/java/orochi/model/Transaction.java
@@ -1,13 +1,13 @@
 package orochi.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@ToString(exclude = "Appointment")
+@EqualsAndHashCode(exclude = "Appointment")
 @Entity
 @Table(name = "Transaction")
 @Data
