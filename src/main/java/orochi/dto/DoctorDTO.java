819195c7d26428d656b367fd25eb306bc8ddc928
NguyenThanhBinh108 <binhnthe182340@fpt.edu.vn>
2025-06-29 00:56:48 +0700

    refactor: update DoctorDTO with additional fields and validation

--- src/main/java/orochi/dto/DoctorDTO.java
+++ src/main/java/orochi/dto/DoctorDTO.java
@@ -1,4 +1,18 @@
 package orochi.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class DoctorDTO {
+    private Integer doctorId;
+    private String bioDescription;
+    private String fullName;
 }
