fb222d930a79e15121f6948959d2c0ad43a93856
NguyenThanhBinh108 <binhnthe182340@fpt.edu.vn>
2025-06-29 00:56:49 +0700

    refactor: update SpecializationImpl with improved business logic

--- src/main/java/orochi/service/impl/SpecializationImpl.java
+++ src/main/java/orochi/service/impl/SpecializationImpl.java
@@ -1,4 +1,30 @@
 package orochi.service.impl;
 
-public class SpecializationImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import orochi.model.Specialization;
+import orochi.repository.SpecializationRepository;
+import orochi.service.SpecializationService;
+
+import java.util.List;
+
+@Service
+public class SpecializationImpl implements SpecializationService {
+
+    private final SpecializationRepository specializationRepository;
+
+    @Autowired
+    public SpecializationImpl(SpecializationRepository specializationRepository) {
+        this.specializationRepository = specializationRepository;
+    }
+
+    @Override
+    public List<Specialization> getAll() {
+        return specializationRepository.findAll();
+    }
+
+    @Override
+    public List<Specialization> getAllSpecializations() {
+        return List.of();
+    }
 }
